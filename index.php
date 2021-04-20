1e44216b4afe0bd8a681db77a1b94b3a599a55e1
romanIntername <roman.liokumovitch@intername.media>
2021-04-20 10:23:25 +0300

    last version

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Purchase Water</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/mainStyle.css">
+</head>
+
+<body>
+
+    <section id="upSection">
+        <div id="bubleSection" class="container">
+            <div class="row">
+                <div class="col-12 title mb-5">
+                    <h1 class="txt mb-5 mt-5">Purchase Water</h1>
+                </div>
+            </div>
+            <div class="row justify-content-center mt-5">
+                <div id="card1" class="col-12 col-md-4 mr-3 btl_card btl_card1">
+                    <img class="img-fluid centerImg btl6" src="./img/6bttls.png" alt="6bottles">
+                    <p class="txt mainText mt-2">6 BOTTLE PACK</p>
+                    <p class="txt price">$2.5 / pack</p>
+                    <div class="btn_inp">
+                        <button type-action="add" price="2.5" product-type='6' class="add ml-1 mr-1"
+                            title="6 bottle pack">+</button>
+                        <input class="inputQuantety" quantety="0" class="quantity1" type="text" readonly=""
+                            placeholder="0">
+                        <button type-action="decrease" product-type='6' price_d="2.5"
+                            class="remove ml-1 mr-1">-</button>
+                    </div>
+                </div>
+                <div id="card2" class="col-12 col-md-4 mr-3 btl_card btl_card2 ">
+                    <img class="img-fluid centerImg btl12" src="./img/12bttls.png" alt="12bottles">
+                    <p class="txt mainText mt-2">12 BOTTLE PACK</p>
+                    <p class="txt price">$4.6 / pack</p>
+                    <div class="btn_inp">
+                        <button type-action="add" product-type='12' price='4.6' class="add ml-1 mr-1"
+                            title="12 bottle pack">+</button>
+                        <input class="inputQuantety" quantety="0" class="quantity2" type="text" readonly=""
+                            placeholder="0">
+                        <button type-action="decrease" product-type='12' price_d="4.6"
+                            class="remove ml-1 mr-1">-</button>
+                    </div>
+
+                </div>
+                <div id="card3" class="col-12 col-md-4 btl_card btl_card3 ">
+                    <img class="img-fluid centerImg btl24" src="./img/24btls.png" alt="24bottles">
+                    <p class="txt mainText mt-2">24 BOTTLE PACK</p>
+                    <p class="txt price">$9 / pack</p>
+                    <div class="btn_inp ">
+                        <button type-action="add" product-type='24' price='9' class="add ml-1 mr-1"
+                            title="24 bottle pack">+</button>
+                        <input class="inputQuantety" quantety="0" class="quantity3" type="text" readonly=""
+                            placeholder="0">
+                        <button type-action="decrease" product-type='24' price_d="9" class="remove ml-1 mr-1">-</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <section id='billingSection'>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-5">
+                    <img class="img-fluid" src="./img/girl_img.png" alt="girl with bottle">
+                </div>
+
+                <div class="col-12 col-md-5">
+                    <h3 class="col-12 p-0">Billing Info</h3>
+                    <form id="mainForm">
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="fname">First Name</label>
+                                <input type="text" class="form-control" id="fname">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="lname">Last Name</label>
+                                <input type="text" class="form-control" id="lname">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" class="form-control" id="email">
+                        </div>
+                        <div class="row">
+                            <div class="col-4" id="biling_cart">
+
+                            </div>
+
+                            <div id="billPackText" class="col-4">
+
+                            </div>
+
+                            <div id=packsQuantenty class="col-4">
+
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-12 col-md-6">
+                                <span id="totalText">Your total :</span>
+                            </div>
+
+                            <div class="col-md-6">
+                                <span id="total"></span>
+                            </div>
+                        </div>
+                        <div class="col align-self-center">
+                            <button type="submit" id="checkout" class="btn btn-primary">Checkout</button>
+                            <p id=errorMsg class="mt-3 mr-3"></p>
+                        </div>
+
+                    </form>
+                </div>
+                <div class="col-12 col-md-2">
+
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <!-- The Modal  -->
+    <div id="myModal" class="modal">
+
+        <!-- Modal content -->
+        <div id="modal-content" class="modal-content">
+            <span class="close">&times;</span>
+
+        </div>
+    </div>
+    <footer>
+
+    </footer>
+
+    <script src="./js/mainScript.js" defer></script>
+</body>
+
+</html>
---
+++ js/mainScript.js
@@ -0,0 +1,167 @@
+let totalPrice = 0;
+let packsArr = [];
+let pursheList = [{
+        "id": "card1",
+        "type": "6",
+        "price": 2.5,
+        "amount": 0,
+        "title": "6 Bottles pack"
+    },
+    {
+        "id": "card2",
+        "type": "12",
+        "price": 4.6,
+        "amount": 0,
+        "title": "12 Bottles pack"
+    },
+    {
+        "id": "card3",
+        "type": "24",
+        "price": 9,
+        "amount": 0,
+        "title": "24 Bottles pack"
+    }
+
+];
+
+let modal = document.getElementById('myModal');
+
+document.querySelectorAll('.btl_card button').forEach(item => {
+    item.addEventListener('click', event => {
+
+        let elem = event.target; //clicked btn
+        let inputElem = elem.parentElement.getElementsByTagName('input')[0]; //input elem
+        let typeProduct = parseInt(elem.getAttribute("product-type")); // 6 ,12 ,24 bottels
+        let quantetyNode = document.createElement("input");
+        let product = pursheList.find(product => product.type == typeProduct);
+        if (elem.getAttribute("type-action") == 'add') {
+            totalPrice += parseFloat(elem.getAttribute('price'))
+            currentQuantety = parseInt(inputElem.getAttribute('quantety'));
+            newQuantety = currentQuantety + 1;
+            inputElem.setAttribute("quantety", newQuantety);
+            inputElem.value = newQuantety;
+            if (product.amount == 0) {
+                //add to product if not in the cart
+                let node = document.createElement("P");
+                node.setAttribute("product-type", typeProduct);
+                let textnode = document.createTextNode(elem.getAttribute('title'));
+                node.append(textnode);
+                document.getElementById("biling_cart").append(node);
+                let packPnode = document.createElement("P");
+                packPnode.setAttribute("product-type", typeProduct);
+                packTextNode = document.createTextNode('Packs Quantity :');
+                packPnode.append(packTextNode);
+                document.getElementById("billPackText").append(packPnode);
+                // let quantetyNode = document.createElement("input");
+                quantetyNode.setAttribute("product-type", typeProduct);
+                quantetyNode.setAttribute("class", "inputBill");
+                quantetyNode.setAttribute("readonly", "");
+                document.getElementById("packsQuantenty").append(quantetyNode);
+
+            }
+            product.amount = newQuantety;
+
+
+        } else if (elem.getAttribute("type-action") == 'decrease' && inputElem.value > 0) {
+            currentQuantety = parseInt(inputElem.getAttribute('quantety'));
+            newQuantety = currentQuantety - 1;
+            inputElem.setAttribute("quantety", newQuantety);
+            totalPrice -= parseFloat(elem.getAttribute('price_d'))
+            inputElem.value = newQuantety;
+            product.amount = newQuantety;
+            quantetyNode.value = product.amount;
+
+            //remove element from list if quantety is 0 of product
+            if (product.amount == 0) {
+                //remove from  product list
+                let removeNodeName = document.querySelector(`P[product-type='${typeProduct}']`);
+                removeNodeName.parentNode.removeChild(removeNodeName);
+                let removeNodeText = document.querySelector(`P[product-type='${typeProduct}']`);
+                removeNodeText.parentNode.removeChild(removeNodeText);
+                let removeNodeInput = document.querySelector(`input[product-type='${typeProduct}']`);
+                removeNodeInput.parentNode.removeChild(removeNodeInput);
+            }
+        }
+
+        quantetyNode.value = newQuantety;
+
+
+
+
+
+
+        totalPrice = Number(totalPrice.toFixed(2));
+        document.getElementById("total").innerHTML = totalPrice;
+
+        //clear total price
+        if (totalPrice == "0") {
+            document.getElementById("total").innerHTML = '';
+        }
+    })
+})
+
+
+//validation and open modal
+document.getElementById("checkout").addEventListener('click', function (event) {
+
+    event.preventDefault();
+    let firstName = document.getElementById("fname").value;
+    let lastName = document.getElementById("lname").value;
+    let email = document.getElementById("email").value;
+    let emailRegex = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$";
+    let lettersRegex = "^[a-zA-Z]{2,70}$";
+    let errorMsg = '';
+    if (firstName.match(lettersRegex)) {
+        if (lastName.match(lettersRegex)) {
+            if (email.length != 0) {
+                if (email.match(emailRegex)) {
+                    let node = document.createElement("P");
+                    node.setAttribute("class", "nameMsg");
+                    let textnode = document.createTextNode(`${firstName}  ${lastName} , you ordered`);
+                    node.append(textnode);
+                    document.getElementById("modal-content").append(node);
+
+                    pursheList.forEach(item => {
+                        if (item.amount > 0) {
+                            let str = `${item.amount} packs of ${item.title}`
+                            node = document.createElement("P");
+                            node.setAttribute("class", 'productMsg');
+                            textnode = document.createTextNode(`${str}`);
+                            node.append(textnode);
+                            document.getElementById("modal-content").append(node);
+
+                        }
+                    })
+                    node = document.createElement("P");
+                    node.setAttribute("class", 'bottom_msg');
+                    textnode = document.createTextNode('Thank You!');
+                    node.append(textnode);
+                    document.getElementById("modal-content").append(node);
+
+                    // Get the button that opens the modal
+                    modal.style.display = 'block';
+                    //close button in modal
+
+                } else {
+                    errorMsg = 'required correct form of email';
+                }
+
+            } else {
+                errorMsg = 'required  email';
+            }
+        } else {
+            errorMsg = 'last name should contain 2-70 letters';
+        }
+    } else {
+        errorMsg = 'first name should contain 2-70 letters';
+    }
+
+    document.getElementById("errorMsg").innerHTML = errorMsg
+});
+
+// modal close
+
+let closeBtn = document.getElementsByClassName("close")[0].addEventListener('click', function () {
+    modal.style.display = "none";
+    location.reload()
+});
